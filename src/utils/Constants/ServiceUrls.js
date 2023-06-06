d811d87f573309bad6ce2015012edeefcd9e34f3
raviDhamini <ravi.dhamani@vlinkinfo.com>
2023-06-06 20:07:27 +0530

    fix: bug fixes

--- src/utils/Constants/ServiceUrls.js
+++ src/utils/Constants/ServiceUrls.js
@@ -1,10 +1,10 @@
 export const serviceUrl = {
   pageSize: 25,
   //For Production
-  // type: 'production',
-  // apiUrl: 'https://greensheep.earth/wp-json/',
+  type: 'production',
+  apiUrl: 'https://greensheep.earth/wp-json/',
 
   //For Staging
-  type: 'staging',
-  apiUrl: 'http://staging.greensheep.earth/wp-json/',
+  // type: 'staging',
+  // apiUrl: 'http://staging.greensheep.earth/wp-json/',
 };
